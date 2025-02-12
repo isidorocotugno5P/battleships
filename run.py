e8eafc46114cbf04bc4a4f3fe0af6ca25eefdf57
isidorocotugno5P <l1sb03u4@students.codeinstitute.net>
2025-02-12 13:51:36 +0000

    Generate basic game structure, board, turns, random ship generator

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# Generating Empty Board
+board = []
+
+for x in range(0,10):
+    board.append(["O"] * 10)
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+        
+def random_row(board):
+    return randint(0,len(board)-1)
+
+def random_col(board):
+    return randint(0,len(board)-1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+for turn in range(8):
+    print("Turn", turn + 1)
+
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Column: "))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("You have sunk a battleship!")
+    else:
+        if (guess_row < 0 or guess_row > 10) or (guess_col < 0 or guess_col > 10):
+            print("You are way off! Try again with numbers equal to or less than 10")
+        elif (board[guess_row][guess_col] == "X"):
+            print("You already guessed that, try again!")
+        else:
+            print("You missed!")
+            board[guess_row][guess_col] = "X"
+            if turn == 10:
+                print("Game Over")
+        print_board(board)
